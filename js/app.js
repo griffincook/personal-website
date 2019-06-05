7456b16f85a31fe1f772224d0e35ab590850fe1b
Griffin Cook <griffincook91@gmail.com>
2019-06-04 22:20:24 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+$(document).on('scroll',function(){
+    if ($(document).scrollTop() > 243) {
+        $('.banner').addClass('fixed');
+    } else {
+        $('.banner').removeClass('fixed');
+    }
+});
+
+$('#indexHeadshot').click(function(){
+    console.log("clicked");
+    $('#indexHeadshot').addClass('spin');
+    setTimeout(function(){
+        $('#indexHeadshot').removeClass('spin');
+    }, 1000)
+});
