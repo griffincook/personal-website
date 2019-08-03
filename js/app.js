42df7159c34535a61d032ed8cfcba813ab2d7e68
Griffin Cook <griffincook91@gmail.com>
2019-08-03 00:20:37 -0400

    made index look better and more responsive

--- js/app.js
+++ js/app.js
@@ -1,15 +1,16 @@
+$('.indexHeadshot').on('click', function(){
+    console.log("clicked");
+    $('.indexHeadshot').addClass('spin');
+    setTimeout(function(){
+        $('.indexHeadshot').removeClass('spin');
+    }, 1000)
+});
+
 $(document).on('scroll',function(){
-    if ($(document).scrollTop() > 243) {
+    let h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0) /4;
+    if ($(document).scrollTop() >= h) {
         $('.banner').addClass('fixed');
     } else {
         $('.banner').removeClass('fixed');
     }
-});
-
-$('#indexHeadshot').on('click', function(){
-    console.log("clicked");
-    $('#indexHeadshot').addClass('spin');
-    setTimeout(function(){
-        $('#indexHeadshot').removeClass('spin');
-    }, 1000)
 });
