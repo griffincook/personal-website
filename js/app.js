c00d8ba1523bd7e32e2f0d986c3e18c9260472d1
Griffin Cook <griffincook91@gmail.com>
2019-08-03 23:51:06 -0400

    added angular and made photo story

--- js/app.js
+++ js/app.js
@@ -1,16 +1,60 @@
-$('.indexHeadshot').on('click', function(){
-    console.log("clicked");
-    $('.indexHeadshot').addClass('spin');
-    setTimeout(function(){
-        $('.indexHeadshot').removeClass('spin');
-    }, 1000)
-});
+(function() {
+    angular.module('index', [])
+        .controller('mainController', function($scope, $timeout) {
+            $scope.pictureClass = "indexHeadshot";
+            $scope.pictureSrc = "images/headshot.png";
+            let flipped = 0;
 
-$(document).on('scroll',function(){
-    let h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0) /4;
-    if ($(document).scrollTop() >= h) {
-        $('.banner').addClass('fixed');
-    } else {
-        $('.banner').removeClass('fixed');
-    }
-});
+            $scope.spinPicture = function() {
+                flipped++;
+                $scope.pictureClass = "spin";
+                $timeout(function(){
+                    $scope.pictureClass = "indexHeadshot";
+                    if (flipped === 1) {
+                        $scope.pictureSrc = "images/headshotFlipped.png";
+                    }
+                    else if (flipped === 2) {
+                        $scope.pictureSrc = "images/headshotText.png";
+                    }
+                    else if (flipped === 3) {
+                        $scope.pictureSrc = "images/headshotFlippedText.png";
+                    }
+                    else if (flipped === 4) {
+                        $scope.pictureSrc = "images/headshotGoofy.png";
+                    }
+                    else if (flipped === 5) {
+                        $scope.pictureSrc = "images/headshotFlippedGoofy.png";
+                    }
+                    else if (flipped === 6) {
+                        $scope.pictureSrc = "images/headshotGoofyText.png";
+                    }
+                    else if (flipped === 7) {
+                        $scope.pictureSrc = "images/headshotFlippedGoofyText.png";
+                        $timeout(function () {
+                            let h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+                            scrollTo({left: 0, top: h, behavior: "smooth"});
+                        }, 500);
+                    }
+                    else if (flipped === 8) {
+                        $scope.pictureSrc = "images/headshotGoofyText2.png";
+                    }
+                    else {
+                        $scope.pictureSrc = "images/headshotCircleEmpty.png";
+                    }
+                }, 500)
+            };
+
+            $scope.navBarCtrl = function() {
+                document.addEventListener("scroll", manageScrollBar);
+            };
+
+            function manageScrollBar() {
+                let h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0) /4;
+                if ($(document).scrollTop() >= h) {
+                    $('.banner').addClass('fixed');
+                } else {
+                    $('.banner').removeClass('fixed');
+                }
+            }
+        });
+})();
