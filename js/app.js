5892141705450314e018573a43819d507b9643a3
Griffin Cook <griffincook91@gmail.com>
2019-09-30 16:47:40 -0400

    added animations, ul, about me details

--- js/app.js
+++ js/app.js
@@ -1,9 +1,51 @@
 (function() {
     angular.module('index', [])
         .controller('mainController', function($scope, $timeout) {
-            $scope.pictureClass = "indexHeadshot";
-            $scope.pictureSrc = "images/headshot_scaled_down.png";
+            init();
             let flipped = 0;
+
+            function init() {
+                $scope.pictureClass = "indexHeadshot";
+                $scope.pictureSrc = "images/headshot_scaled_down.png";
+                $scope.headshotTransparency = "transparent";
+                $scope.titlesTransparency = "transparent";
+                $scope.aboutTransparency = "transparent";
+                $scope.projectTransparency = "transparent";
+                $scope.workTransparency = "transparent";
+                fade();
+            }
+
+            function fade() {
+                $timeout(function(){
+                    $scope.headshotTransparency = "opaque";
+                    $timeout(function(){
+                        $scope.pictureClass = "spin";
+                        $timeout(function(){
+                            $scope.pictureClass = "indexHeadshot";
+                            $scope.pictureSrc = "images/headshot_scaled_down_flipped.png";
+                            $timeout(function(){
+                                $scope.pictureClass = "spin";
+                                $timeout(function(){
+                                    $scope.pictureClass = "indexHeadshot";
+                                    $scope.pictureSrc = "images/headshot_scaled_down.png";
+                                }, 500);
+                            }, 500);
+                        }, 500);
+                    }, 800);
+                }, 800);
+                $timeout(function(){
+                    $scope.titlesTransparency = "opaque";
+                }, 1600);
+                $timeout(function(){
+                    $scope.aboutTransparency = "opaque";
+                }, 2400);
+                $timeout(function(){
+                    $scope.projectTransparency = "opaque";
+                }, 2900);
+                $timeout(function(){
+                    $scope.workTransparency = "opaque";
+                }, 3400);
+            }
 
             $scope.spinPicture = function() {
                 flipped++;
@@ -14,7 +56,7 @@
                         $scope.pictureSrc = "images/headshotFlipped.png";
                     }
                     else if (flipped === 2) {
-                        $scope.pictureSrc = "images/headshotText.png";
+                        $scope.pictureSrc = "images/headshot_scaled_down.png";
                     }
                     else if (flipped === 3) {
                         $scope.pictureSrc = "images/headshotFlippedText.png";
